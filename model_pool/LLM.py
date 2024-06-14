84232efcfffc77c7962ad8072e7d6fc9c4def864
lyr on nuc <scu_luoyr@foxmail.com>
2024-06-14 01:26:05 +0000

    update LLM

--- model_pool/LLM.py
+++ model_pool/LLM.py
@@ -1,5 +1,5 @@
 from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
-# from model_pool.promptTemplate import PROMPT_TEMPLATE_EN, PROMPT_TEMPLATE_ZH
+from model_pool.promptTemplate import PROMPT_TEMPLATE_EN, PROMPT_TEMPLATE_ZH
 import torch
 
 
